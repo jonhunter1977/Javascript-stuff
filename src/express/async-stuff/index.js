8e04100c2c9c42726f7b2df8999720b7db539004
Jon Hunter <jonhunter1977@gmail.com>
2017-06-13 09:04:44 +0100

    got async route working correctly, i think...

--- src/express/async-stuff/index.js
+++ src/express/async-stuff/index.js
@@ -2,28 +2,33 @@
 
 const express = require('express');
 const router = express.Router({
-    mergeParams: true
+  mergeParams: true
 });
-const RouteError = require('../helpers/routeError')
 const noResponseCache = require('../middleware/noResponseCache');
 const waitAndReturnAPromise = require('./lib/waitAndReturnAPromise');
-const asyncPromiseWrapper = require('./lib/asyncPromiseWrapper');
 
 router.get('/status', (req, res, next) => {
-    res.status(200).send('Get OK');
+  res.status(200).send('Get OK');
 });
 
 router.get('/promise/:waitTime?', noResponseCache, (req, res, next) => {
-    const waitTime = req.params.waitTime || 10000;
-    waitAndReturnAPromise(waitTime)
-        .then(() => res.status(200).send('Promise returned'))
+  const waitTime = req.params.waitTime || 10000;
+  waitAndReturnAPromise(waitTime)
+    .then(() => res.status(200).send('Promise returned'))
 });
 
-router.get('/async/:waitTime?', noResponseCache, (req, res, next) => {
-    const waitTime = req.params.waitTime || 10000;
-    asyncPromiseWrapper(waitTime);
-    return res.status(200).send('Async promised wrapper returned');
-
+router.get('/async/:waitTime?', noResponseCache, async (req, res, next) => {
+  const waitTime = req.params.waitTime || 10000;
+  let result;
+  try {
+    result = await waitAndReturnAPromise(waitTime);
+  } catch (e)  {
+    result = e;
+  }
+  console.log(result);
+  return result === true ?
+    res.status(200).send('Async promised wrapper returned') :
+    res.status(500).send(result);
 });
 
 module.exports = router;
--- src/express/async-stuff/lib/waitAndReturnAPromise.js
+++ src/express/async-stuff/lib/waitAndReturnAPromise.js
@@ -1,7 +1,10 @@
 'use strict';
 
 const waitAndReturnAPromise = (msToWait) => {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
+
+    if (isNaN(parseInt(msToWait))) return reject('Invalid wait time');
+
     const wait = msToWait || 5000;
     console.log('timer start - will wait for ', wait);
     setTimeout(() => {
